b57e6cb91a276a5f0d10e0640d0d31709972d9dc
Andrei <An.S-1@yandex.ru>
2025-10-09 19:37:31 +0300

    d-3

---
+++ reading-a-book/src/main/java/ru/epam/learn/main/CoinMain.java
@@ -0,0 +1,19 @@
+package ru.epam.learn.main;
+
+import ru.epam.learn.action.CompareCoin;
+import ru.epam.learn.bean.Coin;
+
+public class CoinMain {
+    public static void main(String[] args) {
+        Coin coin1 = new Coin();
+        coin1.setDiameter(-0.11);
+        coin1.setDiameter(-0.12);
+        coin1.setWeight(150);
+        Coin coin2 = new Coin();
+        coin2.setDiameter(0.21);
+        coin1.setWeight(170);
+        CompareCoin compare = new CompareCoin();
+        compare.compareDiameter(coin1, coin2);
+
+    }
+}
