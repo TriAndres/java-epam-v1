218b492e5ef1fe1c761d2b8eb61f1e71f13cc995
Andrei <An.S-1@yandex.ru>
2025-10-16 18:17:26 +0300

    d-15

--- leetcode/src/main/java/ru/practicum/task_643/Solution.java
+++ leetcode/src/main/java/ru/practicum/task_643/Solution.java
@@ -3,41 +3,43 @@ package ru.practicum.task_643;
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println("Example 1:\n" +
-                "\n" +
+
+        System.out.println("\nExample 1:\n" +
                 "Input: nums = [1,12,-5,-6,50,3], k = 4\n" +
                 "Output: 12.75000\n" +
-                "Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75\n" +
-                "Example 2:\n" +
-                "\n" +
-                "Input: nums = [5], k = 1\n" +
-                "Output: 5.00000");
+                "Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75");
+        int[] nums2 = {1, 12, -5, -6, 50, 3};
+        System.out.println("-> " + findMaxAverage(nums2, 4));
 
-        int[] nums = {1,12,-5,-6,50,3};
-        System.out.println("-> " + findMaxAverage(nums, 4));
-//        int[] nums = {5};
-//        System.out.println("-> " + findMaxAverage(nums, 1));
-
+        System.out.println(
+                "\nInput: nums = [5], k = 1\n" +
+                        "Output: 5.00000");
+        int[] nums1 = {5};
+        System.out.println("-> " + findMaxAverage(nums1, 1));
     }
 
     public static double findMaxAverage(int[] nums, int k) {
-        int n = nums.length;
-        double s = 0;
-        double max = Integer.MAX_VALUE;
-
-        if (n == 1) {
+        double s;
+        double max = Integer.MIN_VALUE;
+        int count = 0;
+        if (nums.length == 1) {
             max = (double) nums[0];
         } else {
-            for (int i = 0; i < n - k; i++) {
+            for (int i = 0; i < nums.length; i++) {
                 s = 0;
-                for (int j = i; j < i + k; j++) {
+                for (int j = i; j < nums.length; j++) {
+                    count++;
                     s += nums[j];
+                    if (count == 4) {
+                        count = 0;
+                        break;
+                    }
                 }
-                max = Math.max(max, s);
-
+                if (max < s) {
+                    max = s;
+                }
             }
         }
-        System.out.println("-> " + max + " -> " + k);
-        return max/k;
+        return max / k;
     }
 }
